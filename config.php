24854f42193443655a024bb2602cf5dd34302586
MAS <digitalcolony@gmail.com>
2017-10-20 20:07:57 -0700

    new version

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    return (object) array(
+        'MP3_DIRECTORY' => 'sounds',
+        'PAGE_TITLE' => 'Soundboard for Neil Rogers, Jennifer Rehm, Jim Mandich, Larry King and more!',
+        // set preload to auto, metadata, none
+        // https://www.w3schools.com/TAGs/att_audio_preload.asp
+        'PRELOAD_AUDIO' => 'none' 
+    );
+?>
