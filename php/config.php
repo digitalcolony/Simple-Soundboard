bb3f2c3ebbe6dcacda4f9bb30e8e72ec1d2f438b
MAS <digitalcolony@gmail.com>
2018-08-31 16:05:32 -0700

    rewrite: Soundboard now loads JSON file and no longer requires PHP. PHP build code decoupled.

---
+++ php/config.php
@@ -0,0 +1,8 @@
+<?php
+    return (object) array(
+        'MP3_DIRECTORY' => '../sounds/',
+        'JSON_FILENAME' => '../inc/json/soundboard.json',
+        'SOUNDBOARD_PAGE' => '/'
+    );
+?>
+
---
+++ php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Build Simple Soundboard JSON File</title>
+</head>
+<body>
+    <h1>Build Simple Soundboard JSON File</h1>
+    <ul>
+        <li><a href="buildJSON_filename.php">File Name Version</a> - Use [Q] for ? and dashes will be replaced with spaces.</li>
+        <li><a href="buildJSON_id3.php">ID3 Version</a> - Use a tool such as <a href="https://www.mp3tag.de/en/">MP3Tag</a> to update the ID3 fields of your MP3s.</li>
+</ul>
+</body>
+</html>
