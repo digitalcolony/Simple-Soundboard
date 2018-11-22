3537d82c3c5e48a62565998c7c0d17f494ad49ab
MAS <digitalcolony@gmail.com>
2018-11-22 10:39:31 -0800

    node filename version

--- node/json_filename.js
+++ node/json_filename.js
@@ -1,7 +1,5 @@
 require("./config/config");
 const path = require("path");
-const mp3Duration = require("mp3-duration");
-
 const fs = require("fs");
 
 String.prototype.replaceAll = function(search, replace) {
@@ -17,32 +15,19 @@ var obj = {
 const getSoundFileName = val => {
   let soundname = path.parse(val).name;
   soundname = soundname.replaceAll("[Q]", "?").replaceAll("-", " ");
-
   return soundname;
-};
-
-const getSoundDuration = val => {
-  mp3Duration(val, (err, duration) => {
-    if (err) {
-      throw err;
-    }
-    console.log(duration);
-    //TODO: Not working. Duration is found, but it is not returning.
-    return duration.toString();
-  });
 };
 
 fs.readdirSync(process.env["MP3_DIRECTORY"]).forEach(file => {
   obj.files.push({
     name: getSoundFileName(file),
-    artist: "",
-    duration: getSoundDuration(process.env["MP3_DIRECTORY"] + file),
-    mp3: file
+    artist: "", // artist is not built with filename version
+    mp3: process.env["MP3_DIRECTORY"] + file.toString()
   });
 });
 
 const json = JSON.stringify(obj);
-fs.writeFile("myjsonfile.json", json, "utf8", function(err) {
+fs.writeFile(process.env["JSON_FILENAME"], json, "utf8", function(err) {
   if (err) {
     throw err;
   }
