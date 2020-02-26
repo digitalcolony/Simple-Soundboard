b1a5397d1b11b44717575b3eadf05bcb338fcff3
Michael Allen Smith <digitalcolony@gmail.com>
2020-02-25 17:46:41 -0800

    add X to clear input

--- inc/js/filter.js
+++ inc/js/filter.js
@@ -1,11 +1,18 @@
 const filterInput = document.getElementById("filterInput");
+const filterClearBtn = document.getElementById("filterClearBtn");
 
 document.addEventListener("DOMContentLoaded", function(event) {
   registerEventListeners();
 });
 
 function registerEventListeners() {
-  filterInput.addEventListener("keyup", filterDrops);
+  filterInput.addEventListener("input", filterDrops);
+  filterClearBtn.addEventListener("click", filterClear);
+}
+
+function filterClear(e){
+  filterInput.value = "";
+  filterInput.dispatchEvent(new Event("input"));
 }
 
 function filterDrops(e) {
